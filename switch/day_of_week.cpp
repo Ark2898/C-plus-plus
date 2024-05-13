c8a836ac6cbc4120a557fc346d45143b3948a320
somu62 <somutiwari62@gmail.com>
2024-05-13 21:15:57 +0530

    TO display the name of a day of the week on its numeric representaion

---
+++ switch/day_of_week.cpp
@@ -0,0 +1,51 @@
+// WAP to display the name of a day of the week based on its numeric representation
+#include<iostream>
+using namespace std;
+int main()
+{
+    int day;
+    cin>>day;
+    switch(day)
+    {
+        case 1:
+        {
+            cout<<"1"<<" sunday";
+            break;
+        }
+        case 2:
+        {
+            cout<<" Monday";
+            break;
+        }
+        case 3:
+        {
+            cout<<" Tuesday";
+            break;
+        }
+        case 4:
+        {
+            cout<<" Wednesday";
+            break;
+        }
+        case 5:
+        {
+            cout<<" Thrusday";
+            break;
+        }
+        case 6:
+        {
+            cout<<" Friday";
+            break;
+        }
+        case 7:
+        {
+            cout<<" Saturday";
+            break;
+        }
+        default:
+        {
+            cout<<" invalid input";
+        }
+    }
+    return 0;
+}
